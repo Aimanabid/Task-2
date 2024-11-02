f79d78adf3dbdd5208136cd59e41daf0c500a01b
Aiman Abid <147660353+Aimanabid@users.noreply.github.com>
2024-11-03 01:19:01 +0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faMessage, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Navbar.css'
 import React from 'react'
@@ -9,8 +9,15 @@ const Navbar = () => {
     
       <div className="d-flex justify-content-between p-3 mt-2">
       <p className='navbartxt'>Sales Lead</p>
-      <div className="search-container">
-      <input type="text" name="search" placeholder="Search.." /> <FontAwesomeIcon icon={faSearch}/> </div>
+      <div className='d-flex'>
+      <div className="search-container mx-2">
+      <input type="text" name="search" placeholder="Search.." /> <FontAwesomeIcon icon={faSearch}/> 
+      </div>
+      <div className='navbaricons mb-2 pt-2 '>
+        <FontAwesomeIcon icon={faBell} className='mx-3 '/>
+        <FontAwesomeIcon icon={faMessage} className='me-3'/>
+        </div>
+      </div>
       </div>
     </>
   )
