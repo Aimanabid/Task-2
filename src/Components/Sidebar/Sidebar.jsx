0b2609d5cb9002e4790b9cc0762e23c26b39739b
Aiman Abid <147660353+Aimanabid@users.noreply.github.com>
2024-11-03 01:21:23 +0500

    Update Sidebar.jsx

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,13 +6,14 @@ import Vector3 from '../../images/Vector (2).svg'
 import Vector4 from '../../images/Vector (3).svg'
 import Vector5 from '../../images/Vector (4).svg'
 import Vector6 from '../../images/Vector (5).svg'
+import image1 from '../../images/Ellipse 22.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faChartLine } from '@fortawesome/free-solid-svg-icons'
+import {  faBell, faChartLine, faMessage, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
     <>
-        <div className='sidebarstick d-flex justify-content-center align-items-center flex-column vh-100 '>
+        <div className='sidebarstick d-flex flex-column vh-100 '>
             <ul className='sidebar text-center '>
                 <li className='my-4'>
                 <div className="d-flex">
@@ -44,7 +45,24 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faChartLine}/>
                 <p className='sidebartxt mx-2'>Report</p>
                 </div></li>
+                <div className='d-flex justify-content-around sidebaricons'>
+                <FontAwesomeIcon icon={faMessage} className=''/>
+                <FontAwesomeIcon icon={faBell} className='mx-4'/>
+                <FontAwesomeIcon icon={faQuestionCircle}/>
+                </div>
             </ul>
+            <div className='d-flex'>
+          <div className='mt-4'>
+                <img src={image1} alt="personImg"  />
+                </div>
+                <div>
+                  <h4 className='sidebartxt2'>Unknown</h4>
+                  <p className='sidebartxt3'>unknown@gmail.com</p>
+                </div>
+                </div>
+                <div>
+                  <p className='sidebartxt4'>© 2024 centrixhub.ai</p>
+                </div>
         </div>
       
     </>
